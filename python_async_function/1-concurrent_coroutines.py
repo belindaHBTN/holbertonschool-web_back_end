2aa1374889d227dbbda239dcb6519d5dd8829e9e
belindaHBTN <6372@holbertonstudents.com>
2023-08-30 04:52:56 -0700

    added annotation for return

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
 """an asynchronous coroutine"""
+from typing import List
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(n: int, max_delay: int):
+async def wait_n(n: int, max_delay: int) -> List[float]:
     """execute multiple coroutines at the same time"""
     delay_list = []
     for i in range(n):
