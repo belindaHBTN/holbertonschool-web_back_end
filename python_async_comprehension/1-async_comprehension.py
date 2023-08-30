f1a2df15da5f8c74c05c08bd1d2fb4db36778a2e
belindaHBTN <6372@holbertonstudents.com>
2023-08-30 07:20:36 -0700

    updated format

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -8,7 +8,5 @@ async def async_comprehension() -> List[float]:
     """return the list of numbers"""
     async_generator = __import__('0-async_generator').async_generator
 
-    num_list = []
-    async for number in async_generator():
-        num_list.append(number)
+    num_list = [number async for number in async_generator()]
     return num_list
