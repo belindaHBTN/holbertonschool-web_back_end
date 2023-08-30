44f8f807426b408b1947ff2a46daa665ffec73c8
belindaHBTN <6372@holbertonstudents.com>
2023-08-30 06:50:36 -0700

    added annotation

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -2,9 +2,10 @@
 """an asynchronous coroutine"""
 import asyncio
 import random
+from typing import Generator
 
 
-async def async_generator():
+async def async_generator() -> Generator[float, None, None]:
     """loop 10 times, return random number"""
     for i in range(10):
         await asyncio.sleep(1)
