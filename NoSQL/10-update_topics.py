7e2a280bea4861295844485dc31cde15442e869f
belindaHBTN <6372@holbertonstudents.com>
2023-09-04 22:28:54 -0700

    updated to match many

--- NoSQL/10-update_topics.py
+++ NoSQL/10-update_topics.py
@@ -3,4 +3,4 @@
 
 def update_topics(mongo_collection, name, topics):
     """update topics"""
-    mongo_collection.update_one({"name":name}, {"$set": {"topics":topics}})
+    mongo_collection.update_many({"name":name}, {"$set": {"topics":topics}})
