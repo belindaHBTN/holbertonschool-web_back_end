4ae1fd108b4b8b13d31f422caddefa0d2280a00a
belindaHBTN <6372@holbertonstudents.com>
2023-09-05 22:30:44 -0700

    12. Log stats

---
+++ NoSQL/12-log_stats.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""a Python script that provides stats about Nginx logs"""
+from pymongo import MongoClient
+
+
+def nginx_stats(collection):
+    """display some stats about Nginx logs stored in MongoDB"""
+    logs = collection.count_documents({})
+    print(f'{logs} logs')
+
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    for method in methods:
+        print(f'method {method}: {collection.count_documents({"method":method})}')
+
+    status = collection.count_documents({"method":"GET", "path":"/status"}) 
+    print(f'{status} status check')
+
+
+if __name__ == "__main__":
+    client = MongoClient('mongodb://127.0.0.1:27017')
+    nginx_collection = client.logs.nginx
+    nginx_stats(nginx_collection)
+
