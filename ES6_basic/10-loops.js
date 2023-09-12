d6da82153d2ddddbf6923c2e7c70dc74eb701fda
belindaHBTN <6372@holbertonstudents.com>
2023-09-12 12:42:11 +0000

    10. For...of Loops

---
+++ ES6_basic/10-loops.js
@@ -0,0 +1,9 @@
+export default function appendToEachArrayValue(array, appendString) {
+  let idx = 0;
+  const newArray = [];
+  for (const value of array) {
+    newArray[idx] = appendString + value;
+    idx += 1;
+  }
+  return newArray;
+}
