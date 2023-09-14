0c707becb2da1ac45541adf7480ea5d38d34bfcf
belindaHBTN <6372@holbertonstudents.com>
2023-09-14 11:46:04 +0000

    2. A Course, Getters, and Setters

---
+++ ES6_classes/2-hbtn_course.js
@@ -0,0 +1,40 @@
+export default class HolbertonCourse {
+  constructor(name, length, students) {
+    this._name = name;
+    this._length = length;
+    this._students = students;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(newName) {
+    if (typeof newName !== 'string') {
+      throw new Error('Name must be a string');
+    }
+    this._name = newName;
+  }
+
+  get length() {
+    return this._length;
+  }
+
+  set length(newLength) {
+    if (typeof newLength !== 'number') {
+      throw new Error('Length must be a number');
+    }
+    this._length = newLength;
+  }
+
+  get students() {
+    return this._students;
+  }
+
+  set students(newStudents) {
+    if (typeof newStudents !== 'string') {
+      throw new Error('Students must be an array of Strings');
+    }
+    this._students = newStudents;
+  }
+}
