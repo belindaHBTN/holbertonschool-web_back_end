c0bcb1ef04390bc2e41e56100ee1f27e399e50bf
belindaHBTN <6372@holbertonstudents.com>
2023-10-02 20:54:46 +1100

    changed to promise.then.catch

--- ES6_promise/3-all.js
+++ ES6_promise/3-all.js
@@ -1,12 +1,12 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default async function handleProfileSignup() {
-  try {
-    const photo = (await uploadPhoto()).body;
-    const name = (await createUser());
-    const { firstName, lastName } = name;
-    console.log(photo, firstName, lastName);
-  } catch (e) {
+  const promise1 = uploadPhoto();
+  const promise2 = createUser();
+
+  return Promise.all([promise1, promise2]).then((results) => {
+    console.log(`${results[0].body} ${results[1].firstName} ${results[1].lastName}`);
+  }).catch(() => {
     console.log('Signup system offline');
-  }
+  });
 }
