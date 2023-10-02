fa18e048bacbcb22abd1cf381fa054a37ea7c674
belindaHBTN <6372@holbertonstudents.com>
2023-10-02 22:52:52 +1100

    updated the format

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -1,14 +1,16 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
+function handlePromise(p) {
+  return p.then((status) => ({ status: 'fulfilled', value: status }))
+    .catch((e) => ({ status: 'rejected', value: `Error: ${e.message}` }));
+}
+
 export default function handleProfileSignup(firstName, lastName, fileName) {
   const promise1 = signUpUser(firstName, lastName);
   const promise2 = uploadPhoto(fileName);
 
-  return Promise.all([promise1, promise2]).then((results) => (
-    results.forEach((result) => ({
-      status: result.status,
-      value: result.value,
-    }))
-  )).catch((e) => [{ status: 'rejected', value: e }]);
+  const promises = [promise1, promise2].map(handlePromise);
+
+  return Promise.all(promises);
 }
