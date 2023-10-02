a96f16afc87475a2bfc3f6e7dba8f29f4770a20b
belindaHBTN <6372@holbertonstudents.com>
2023-10-02 17:44:55 +1100

    changed the order of attributes in obj

--- ES6_promise/2-then.js
+++ ES6_promise/2-then.js
@@ -1,5 +1,5 @@
 export default function handleResponseFromAPI(promise) {
-  return promise.then(() => ({ status: 200, body: 'Success' }))
+  return promise.then(() => ({ body: 'success', status: 200 }))
     .catch(() => new Error())
     .finally(console.log('Got a response from the API'));
 }
