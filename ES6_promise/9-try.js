d3759a3e11d4b4f97524a9990ad3272a7dc9f75f
belindaHBTN <6372@holbertonstudents.com>
2023-10-03 12:21:30 +1100

    edited the error message

--- ES6_promise/9-try.js
+++ ES6_promise/9-try.js
@@ -4,7 +4,7 @@ export default function guardrail(mathFunction) {
     const result = mathFunction();
     queue.push(result);
   } catch (e) {
-    queue.push(e.message);
+    queue.push(`Error: ${e.message}`);
   }
   queue.push('Guardrail was processed');
   return queue;
