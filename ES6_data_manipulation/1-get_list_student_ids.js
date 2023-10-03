ca9439d5497fd4b91a8f1218591a9d4a27372571
belindaHBTN <6372@holbertonstudents.com>
2023-10-03 13:29:05 +1100

    updated the array type check

--- ES6_data_manipulation/1-get_list_student_ids.js
+++ ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,5 +1,5 @@
 export default function getListStudentIds(students) {
-  if (typeof students === 'object') {
+  if (Array.isArray(students)) {
     return students.map((student) => student.id);
   }
   return [];
